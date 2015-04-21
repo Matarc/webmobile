75784db8a5a344e5d4005d615a31dfffd9e02754
hilaire.mathieu@gmail.com <hilaire.mathieu@gmail.com>
2015-04-21 16:01:35 +0200

    Ajout des fichiers du projet

---
+++ vues/pageoffre.php
@@ -0,0 +1,20 @@
+<div data-role="page" id="pageoffre"> 
+<?php
+include "vues/backbutton.html";
+include "vues/entetepage.html";
+?>
+ <div data-role="content" >  
+     <div id="offre">
+         Page en construction
+ </div>
+    
+    
+    
+    
+    
+    
+ </div><!-- /content -->
+<?php
+    include "vues/pied.html";
+?>
+</div><!-- /page -->
